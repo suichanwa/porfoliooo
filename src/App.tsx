a9414e02317973c40456f36d244cf60995493f52
suichanwa <augustinnegura746@gmail.com>
2025-02-09 23:52:04 +0200

    upd small

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import { ThemeProvider } from './context/ThemeContext';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/index';
+import Blog from './pages/blog';
 import './App.css';
 
 function App() {
   return (
     <ThemeProvider>
-      <Home />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/blog" element={<Blog />} />
+        </Routes>
+      </Router>
     </ThemeProvider>
   );
 }
