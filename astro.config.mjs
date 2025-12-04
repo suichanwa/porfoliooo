02bba71af9475efb8f44168853f7925d9c25bf51
suichanwa <augustinnegura746@gmail.com>
2025-12-04 18:01:03 +0200

    Switch to GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://suichanwa.github.io',
+  base: '/porfoliooo',
   integrations: [
     react({
       include: ['**/react/*', '**/Physics/*'],
