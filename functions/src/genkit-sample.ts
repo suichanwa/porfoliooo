4385606bc881d49d0f0383d86a6990226b7cbb64
suichanwa <augustinnegura746@gmail.com>
2026-03-25 00:43:07 +0200

    upd minor things

--- functions/src/genkit-sample.ts
+++ functions/src/genkit-sample.ts
@@ -6,7 +6,7 @@ import {genkit, z} from "genkit";
 // function from a Genkit action. It automatically implements streaming if your flow does.
 // The https library also has other utility methods such as hasClaim, which verifies that
 // a caller's token has a specific claim (optionally matching a specific value)
-import { onCallGenkit, hasClaim } from "firebase-functions/https";
+import { onCallGenkit } from "firebase-functions/https";
 
 // Genkit models generally depend on an API key. APIs should be stored in Cloud Secret Manager so that
 // access to these sensitive values can be controlled. defineSecret does this for you automatically.
